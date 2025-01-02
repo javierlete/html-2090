00a1aacfb3ca2ea73f24d20af25f15b9c2e2644a
javierlete <javierlete@localhost>
2025-01-02 16:38:34 +0100

    Corrección de buscar para que mande usuarios también

--- JAVA/ipartube/src/main/java/ipartube/controladores/BuscarServlet.java
+++ JAVA/ipartube/src/main/java/ipartube/controladores/BuscarServlet.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import ipartube.modelos.Usuario;
 import ipartube.modelos.Video;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -21,16 +22,24 @@ public class BuscarServlet extends HttpServlet {
 			throws ServletException, IOException {
 		String texto = request.getParameter("texto");
 
-		String sql = "SELECT * FROM videos WHERE titulo LIKE '%" + texto + "%'";
+		String sql = "SELECT * FROM videos v JOIN usuarios u ON v.id_usuario = u.id WHERE titulo LIKE '%" + texto
+				+ "%'";
 
 		ArrayList<Video> videos = new ArrayList<Video>();
 
 		try (Connection con = BaseDeDatos.conectar(); ResultSet rs = BaseDeDatos.consultar(con, sql)) {
 			Video video = null;
+			Usuario usuario = null;
 
 			while (rs.next()) {
+				usuario = new Usuario(rs.getInt("id_usuario"), rs.getString("nombre"), rs.getString("email"),
+						rs.getString("password"), rs.getString("rol"));
+
 				video = new Video(rs.getInt("id"), rs.getString("titulo"), rs.getString("descripcion"),
 						rs.getString("url"));
+				
+				video.setUsuario(usuario);
+
 				videos.add(video);
 			}
 		} catch (SQLException e) {
